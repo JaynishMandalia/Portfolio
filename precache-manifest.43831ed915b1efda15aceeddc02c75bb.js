5c17a99ac3c32022ad1f85330b0df821be908c15
Jaynish Mandalia <mandaliasj22@hotmail.com>
2022-06-06 12:19:45 +0530

    Updates

---
+++ precache-manifest.43831ed915b1efda15aceeddc02c75bb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "58d83b0b26c119bf54a7",
+    "url": "/Portfolio/static/css/main.38838fa5.chunk.css"
+  },
+  {
+    "revision": "58d83b0b26c119bf54a7",
+    "url": "/Portfolio/static/js/main.58d83b0b.chunk.js"
+  },
+  {
+    "revision": "4a5cc6229667cbe21dc0",
+    "url": "/Portfolio/static/js/1.4a5cc622.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "068aa4a1654ebbeb83fc678f66cdb23a",
+    "url": "/Portfolio/static/media/Jaynish Mandalia.068aa4a1.pdf"
+  },
+  {
+    "revision": "6c77b07ae224cb7ecd6b7be1976ec3e0",
+    "url": "/Portfolio/static/media/Me.6c77b07a.png"
+  },
+  {
+    "revision": "b6948a54b8b426a8ffe52a489929331b",
+    "url": "/Portfolio/static/media/Image.b6948a54.jpg"
+  },
+  {
+    "revision": "eb6bf35445cfe4c98def14bc1c4849a7",
+    "url": "/Portfolio/static/media/NFT.eb6bf354.png"
+  },
+  {
+    "revision": "4f3652a2af3a2cc1f4409180d8fc4794",
+    "url": "/Portfolio/static/media/OnlineShop.4f3652a2.png"
+  },
+  {
+    "revision": "631a19fd79989fe3136ea3f4f4b93813",
+    "url": "/Portfolio/static/media/api.631a19fd.png"
+  },
+  {
+    "revision": "d00abadf9dc6f5fa21e676c8989a27ed",
+    "url": "/Portfolio/static/media/SocialMedia.d00abadf.png"
+  },
+  {
+    "revision": "4fe9bf2aa9cd699c17889b0e8d607652",
+    "url": "/Portfolio/static/media/GoogleKeep.4fe9bf2a.jpg"
+  },
+  {
+    "revision": "c72c08c020f3db9a9dfc14f4e2f630b5",
+    "url": "/Portfolio/index.html"
+  }
+];
