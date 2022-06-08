c83538e226e56cbc5279782c6796666cf3fa4b08
Jaynish Mandalia <mandaliasj22@hotmail.com>
2022-06-08 16:53:09 +0530

    Changes Done

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import CV from "../../assets/Jaynish Mandalia.pdf";
 const CTA = () => {
   return (
-      <div className='cta'>
-          <a href={CV} target="_blank" rel="noopener noreferrer" className='btn'>Download CV</a>
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    <div className='cta'>
+      <a href={CV} target="_blank" rel="noopener noreferrer" className='btn'>Download CV</a>
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
